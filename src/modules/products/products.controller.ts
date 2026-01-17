870ff6e6ddbf9293ca06e2f6fb965925671a1200
JFernandoB <josefernandobolivar2@gmail.com>
2026-01-17 14:41:37 -0400

    feat: hacer publica la lista de productos

--- src/modules/products/products.controller.ts
+++ src/modules/products/products.controller.ts
@@ -12,29 +12,36 @@ import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { Auth } from 'src/modules/auth/decorators/auth.decorator';
 import { Role } from 'src/common/enums/role.enum';
-import { ApiBasicAuth } from '@nestjs/swagger';
+import { ApiBasicAuth, ApiTags } from '@nestjs/swagger';
 
-@ApiBasicAuth()
-@Auth(Role.SUPERVISOR)
+@ApiTags('products')
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
-  @Post()
-  create(@Body() createProductDto: CreateProductDto) {
-    return this.productsService.create(createProductDto);
-  }
-
+  // MÉTODO PÚBLICO: No tiene el decorador @Auth
   @Get()
   findAll() {
     return this.productsService.findAll();
   }
 
+  // MÉTODO PÚBLICO: No tiene el decorador @Auth
   @Get(':id')
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.productsService.findOneBy(id);
   }
 
+  // MÉTODO PROTEGIDO: Solo Supervisor
+  @ApiBasicAuth()
+  @Auth(Role.SUPERVISOR)
+  @Post()
+  create(@Body() createProductDto: CreateProductDto) {
+    return this.productsService.create(createProductDto);
+  }
+
+  // MÉTODO PROTEGIDO: Solo Supervisor
+  @ApiBasicAuth()
+  @Auth(Role.SUPERVISOR)
   @Patch(':id')
   update(
     @Param('id', ParseIntPipe) id: number,
